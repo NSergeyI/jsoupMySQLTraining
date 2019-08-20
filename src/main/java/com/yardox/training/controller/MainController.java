002e39e3ad041a700c89ffa9ff7d787385eaed95
SNikitinI <40792295+SNikitinI@users.noreply.github.com>
2019-08-20 16:05:58 +0300

    add yarnovosti

--- src/main/java/com/yardox/training/controller/MainController.java
+++ src/main/java/com/yardox/training/controller/MainController.java
@@ -1,4 +1,21 @@
 package com.yardox.training.controller;
 
+import com.yardox.training.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MainController {
+
+    @Autowired
+    NewsService newsService;
+
+    @GetMapping({"/","/hello"})
+    public String hello(Model model,@RequestParam(value="name", required=false, defaultValue="World") String name){
+        model.addAttribute("name",newsService.parse() );
+        return "hello";
+    }
 }
--- src/main/java/com/yardox/training/service/NewsService.java
+++ src/main/java/com/yardox/training/service/NewsService.java
@@ -1,7 +1,38 @@
 package com.yardox.training.service;
 
+import com.yardox.training.repos.NewsRepo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 
 @Service
 public class NewsService {
+
+    private static final Logger LOGGER = LogManager.getLogger(NewsService.class);
+
+    @Autowired
+    private NewsRepo newsRepo;
+
+    public String parse() {
+        String result = "";
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://yarnovosti.com").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements newsHeadlines = doc.getElementsByClass("linkName");
+        for (Element headline : newsHeadlines) {
+            result = result + headline + "\n";
+        }
+        return result;
+    }
 }
