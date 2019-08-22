1573edc960def6374736e05a1929734c94e34d44
SNikitinI <40792295+SNikitinI@users.noreply.github.com>
2019-08-22 14:01:26 +0300

    add getNews

--- src/main/java/com/yardox/training/service/NewsService.java
+++ src/main/java/com/yardox/training/service/NewsService.java
@@ -1,6 +1,11 @@
 package com.yardox.training.service;
 
+import com.yardox.training.domain.Author;
+import com.yardox.training.domain.News;
+import com.yardox.training.domain.Tag;
+import com.yardox.training.repos.AuthorRepo;
 import com.yardox.training.repos.NewsRepo;
+import com.yardox.training.repos.TagRepo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -12,6 +17,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
 @Service
 public class NewsService {
@@ -21,6 +27,12 @@ public class NewsService {
     @Autowired
     private NewsRepo newsRepo;
 
+    @Autowired
+    private AuthorRepo authorRepo;
+
+    @Autowired
+    private TagRepo tagRepo;
+
     public String parse() {
         String result = "";
         Document doc = null;
@@ -29,10 +41,35 @@ public class NewsService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Elements newsHeadlines = doc.getElementsByClass("linkName");
+        Elements newsHeadlines = doc.getElementsByTag("article");
         for (Element headline : newsHeadlines) {
-            result = result + headline + "\n";
+            result = result + getNews(headline) + "\n";
         }
+        return result;
+    }
+
+    private News getNews(Element element) {
+        String title = element.getElementsByClass("content").text();
+        Timestamp date = new Timestamp(1000000);
+        String text = element.getElementsByClass("caption").text();
+        Tag tag = getTag(element);
+        Author author = getAuthor(element);
+        News result = new News(title, date, text, tag, author);
+        result = newsRepo.save(result);
+        return result;
+    }
+
+    private Author getAuthor(Element element) {
+        String name = "";
+        Author result = new Author(name);
+        result = authorRepo.save(result);
+        return result;
+    }
+
+    private Tag getTag(Element element) {
+        String name = element.getElementsByClass("categories").text();
+        Tag result = new Tag(name);
+        result = tagRepo.save(result);
         return result;
     }
 }
