1005d2b324409bc01e6af2cfdfb57e3b74747562
SNikitinI <40792295+SNikitinI@users.noreply.github.com>
2019-08-22 16:12:38 +0300

    for home

--- src/main/java/com/yardox/training/service/NewsService.java
+++ src/main/java/com/yardox/training/service/NewsService.java
@@ -18,6 +18,10 @@ import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 @Service
 public class NewsService {
@@ -33,14 +37,10 @@ public class NewsService {
     @Autowired
     private TagRepo tagRepo;
 
+
     public String parse() {
         String result = "";
-        Document doc = null;
-        try {
-            doc = Jsoup.connect("https://yarnovosti.com").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Document doc = getDocument("https://yarnovosti.com");
         Elements newsHeadlines = doc.getElementsByTag("article");
         for (Element headline : newsHeadlines) {
             result = result + getNews(headline) + "\n";
@@ -49,13 +49,37 @@ public class NewsService {
     }
 
     private News getNews(Element element) {
-        String title = element.getElementsByClass("content").text();
-        Timestamp date = new Timestamp(1000000);
-        String text = element.getElementsByClass("caption").text();
+        String link = element.getElementsByClass("linkName").first().getElementsByTag("a").attr("href");
+        Document doc = getDocument(link);
+        Element newsHeadlines = doc.getElementsByClass("news current").first();
+        String title = newsHeadlines.getElementsByClass("listHeader").text();
+        String text = getText(newsHeadlines);
+        Timestamp date = getTimestamp(element);
+//        element.getElementsByClass("caption").text();
         Tag tag = getTag(element);
         Author author = getAuthor(element);
         News result = new News(title, date, text, tag, author);
         result = newsRepo.save(result);
+//        newsHeadlines.notifyAll();
+        return result;
+    }
+
+    private String getText(Element newsHeadlines) {
+        String result = "";
+        Elements elements = newsHeadlines.getElementsByTag("p");
+        for (Element element : elements) {
+            result = result + element.text() + "\n";
+        }
+        return result;
+    }
+
+    private Document getDocument(String link) {
+        Document result = null;
+        try {
+            result = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return result;
     }
 
@@ -70,6 +94,18 @@ public class NewsService {
         String name = element.getElementsByClass("categories").text();
         Tag result = new Tag(name);
         result = tagRepo.save(result);
+        return result;
+    }
+
+    private Timestamp getTimestamp(Element element) {
+        Timestamp result;
+        if (element.getElementsByTag("time").text().isEmpty()) {
+            result = Timestamp.valueOf(LocalDateTime.now());
+        } else {
+            String time = element.getElementsByTag("time").text();
+            LocalDateTime localDateTime = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("dd.MM.uuuu HH:mm"));
+            result = Timestamp.valueOf(localDateTime);
+        }
         return result;
     }
 }
