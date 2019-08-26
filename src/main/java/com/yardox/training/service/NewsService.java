3439a1fbf7e449668efca82c80b1315a2f4c130f
SNikitinI <40792295+SNikitinI@users.noreply.github.com>
2019-08-26 16:41:25 +0300

    for home

--- src/main/java/com/yardox/training/service/NewsService.java
+++ src/main/java/com/yardox/training/service/NewsService.java
@@ -37,7 +37,6 @@ public class NewsService {
     @Autowired
     private TagRepo tagRepo;
 
-
     public String parse() {
         String result = "";
         Document doc = getDocument("https://yarnovosti.com");
@@ -45,30 +44,14 @@ public class NewsService {
         for (Element headline : newsHeadlines) {
             result = result + getNews(headline) + "\n";
         }
-        return result;
+        return "11";
     }
 
     private News getNews(Element element) {
         String link = element.getElementsByClass("linkName").first().getElementsByTag("a").attr("href");
-        Document doc = getDocument(link);
-        Element newsHeadlines = doc.getElementsByClass("_content").first();
-        String title = newsHeadlines.getElementsByClass("listHeader").text();
-        String text = getText(newsHeadlines);
-        Timestamp date = getTimestamp(element);
-        Tag tag = getTag(element);
-        Author author = getAuthor(newsHeadlines);
-        News result = new News(title, date, text, link, tag, author);
-        result = newsRepo.save(result);
-        return result;
-    }
-
-    private String getText(Element newsHeadlines) {
-        String result = "";
-        Elements elements = newsHeadlines.getElementsByTag("p");
-        for (Element element : elements) {
-            result = result + element.text() + "\n";
-        }
-        return result;
+        Runnable runnable = new ThreadService(link);
+        new Thread(runnable).start();
+        return null;
     }
 
     private Document getDocument(String link) {
@@ -77,38 +60,6 @@ public class NewsService {
             result = Jsoup.connect(link).get();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        return result;
-    }
-
-    private Author getAuthor(Element element) {
-        String name = "";
-        Author result = authorRepo.findByName(name);
-        if (result == null) {
-            result = new Author(name);
-            result = authorRepo.save(result);
-        }
-        return result;
-    }
-
-    private Tag getTag(Element element) {
-        String name = element.getElementsByClass("categories").text();
-        Tag result = tagRepo.findByName(name);
-        if (result == null) {
-            result = new Tag(name);
-            result = tagRepo.save(result);
-        }
-        return result;
-    }
-
-    private Timestamp getTimestamp(Element element) {
-        Timestamp result;
-        if (element.getElementsByTag("time").text().isEmpty()) {
-            result = Timestamp.valueOf(LocalDateTime.now());
-        } else {
-            String time = element.getElementsByTag("time").text();
-            LocalDateTime localDateTime = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("dd.MM.uuuu HH:mm"));
-            result = Timestamp.valueOf(localDateTime);
         }
         return result;
     }
--- src/main/java/com/yardox/training/service/ThreadService.java
+++ src/main/java/com/yardox/training/service/ThreadService.java
@@ -1,4 +1,107 @@
 package com.yardox.training.service;
 
-public class ThreadService {
+import com.yardox.training.domain.Author;
+import com.yardox.training.domain.News;
+import com.yardox.training.domain.Tag;
+import com.yardox.training.repos.AuthorRepo;
+import com.yardox.training.repos.NewsRepo;
+import com.yardox.training.repos.TagRepo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class ThreadService implements Runnable {
+
+    String link;
+
+    private static final Logger LOGGER = LogManager.getLogger(ThreadService.class);
+
+    @Autowired
+    private NewsRepo newsRepo;
+
+    @Autowired
+    private AuthorRepo authorRepo;
+
+    @Autowired
+    private TagRepo tagRepo;
+
+    public ThreadService() {
+    }
+
+    public ThreadService(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public void run() {
+
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Element newsHeadlines = doc.getElementsByClass("_content").first();
+        String title = newsHeadlines.getElementsByClass("listHeader").text();
+        String text = getText(newsHeadlines);
+        Timestamp date = getTimestamp(newsHeadlines);
+        Tag tag = getTag(newsHeadlines);
+        Author author = getAuthor(newsHeadlines);
+        News result = new News(title, date, text, link, tag, author);
+        result = newsRepo.save(result);
+    }
+
+    private String getText(Element newsHeadlines) {
+        String result = "";
+        Elements elements = newsHeadlines.getElementsByTag("p");
+        for (Element element : elements) {
+            result = result + element.text() + "\n";
+        }
+        return result;
+    }
+
+    private Timestamp getTimestamp(Element element) {
+        Timestamp result;
+        if (element.getElementsByTag("time").text().isEmpty()) {
+            result = Timestamp.valueOf(LocalDateTime.now());
+        } else {
+            String time = element.getElementsByTag("time").text();
+//            LocalDateTime localDateTime = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("dd.MM.uuuu HH:mm"));
+            LocalDateTime localDateTime = LocalDateTime.now();
+            result = Timestamp.valueOf(localDateTime);
+        }
+        return result;
+    }
+
+    synchronized private Author getAuthor(Element element) {
+        String name = "";
+        Author result = authorRepo.findByName(name);
+        if (result == null) {
+            result = new Author(name);
+            result = authorRepo.save(result);
+        }
+        return result;
+    }
+
+    synchronized private Tag getTag(Element element) {
+        String name = element.getElementsByClass("categories").text();
+        LOGGER.info(name);
+        Tag result = tagRepo.findByName(name);
+        if (result == null) {
+            result = new Tag(name);
+            result = tagRepo.save(result);
+        }
+        return result;
+    }
 }
